6c2bae2bbd6322b1137029123c242cd3848b1ddf
jeha-dev <leejeha.dev@gmail.com>
2023-06-02 00:09:14 +0900

    퍼블) 크루참여하기 페이지 업로드

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import Header from "./components/layouts/Header";
-import Bottom from "./components/layouts/Bottom";
+// import Bottom from "./components/layouts/Bottom";
+import JoinCrew from "./components/view/JoinCrew";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <div id="layoutWrap">
         <Header />
-        <main>
+        {/* <main>
           <h1>App</h1>
           <p>Count: {count}</p>
           <button onClick={() => setCount(count + 1)}>Increment</button>
-        </main>
-        <Bottom />
+        </main> */}
+        <JoinCrew />
+        {/* <Bottom /> */}
       </div>
     </>
   );
--- src/components/view/JoinCrew.jsx
+++ src/components/view/JoinCrew.jsx
@@ -1,22 +1,55 @@
 // import;
 import classes from "./JoinCrew.module.css";
 import visualBannerCrew from "../../assets/images/banner_01.png";
+import profileSampleDog from "../../assets/images/profile_sample_01.png";
+import Heart from "../ui/Heart.jsx";
 
 function JoinCrew() {
   return (
     <>
-      <section className={classes.visualBanner}>
+      <section>
+        {/* 배너 */}
         <div className={classes.bannerBox}>
           <img src={visualBannerCrew} />
         </div>
+        {/* 크루정보 */}
         <div className={classes.crewInfo}>
-          <span>한강지킴이 모여라!</span>
+          <p className={classes.crewIntroTitle}>한강지킴이 모여라!</p>
           <div className={classes.tagBox}>
-            <span>#한강</span>
-            <span>#한강</span>
-            <span>#한강</span>
+            <span className={classes.tags}>#한강</span>
+            <span className={classes.tags}>#저녁런</span>
+            <span className={classes.tags}>#담깅</span>
           </div>
-          <div className={classes.crewProfile}></div>
+          <div className={classes.crewProfile}>
+            <div className={classes.photoBox}>
+              <img src={profileSampleDog} />
+            </div>
+            <div className={classes.detailCrewInfo}>
+              <p>
+                <strong className={classes.crewName}>한강수호대</strong>
+                <span className={classes.markGray}>프로필</span>
+              </p>
+              <div className={classes.crewNum}>
+                <span className={classes.current}>3</span>
+                <span>/</span>
+                <span className={classes.total}>15</span>
+                <span>&nbsp;&nbsp;명</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 버튼 영역 */}
+        <div className={classes.btnWrap}>
+          <button type="button" className={classes.btnHeart}>
+            <span className={classes.ico}>
+              <Heart />
+            </span>
+            <span className={classes.num}>600</span>
+          </button>
+          <button type="button" className={`${classes.btn} ${classes.green}`}>
+            참여하기
+          </button>
         </div>
       </section>
     </>
