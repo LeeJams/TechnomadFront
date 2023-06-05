0654d6d4692ee39d31138504d6b371685e8705ba
LeeJams <woals214@gmail.com>
2023-06-05 09:56:50 +0900

    bottom Link -> NavLink

--- src/components/layouts/Bottom.jsx
+++ src/components/layouts/Bottom.jsx
@@ -4,13 +4,11 @@ import IcoCrew from "../ui/IcoCrew.jsx";
 import IcoHome from "../ui/IcoHome.jsx";
 import IcoChallenge from "../ui/IcoChallenge.jsx";
 import IcoPerson from "../ui/IcoPerson.jsx";
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Bottom() {
-  const { pathname } = useLocation();
-
-  const selectColor = (path) => {
-    return pathname.includes(path) ? "#1EC41E" : "#999";
+  const selectColor = (active) => {
+    return active ? "#1EC41E" : "#999";
   };
 
   return (
@@ -18,34 +16,42 @@ function Bottom() {
       <div className={classes.bottomBar}>
         <ul className={classes.menuBox}>
           <li className={classes.listMenu}>
-            <Link to="/crew">
-              <span className={classes.menuIcon}>
-                <IcoCrew onGreen={selectColor("/crew")} />
-              </span>
-              <span
-                className={classes.menuName}
-                style={{
-                  color: selectColor("/crew"),
-                }}
-              >
-                크루
-              </span>
-            </Link>
+            <NavLink to="/crew">
+              {({ isActive }) => (
+                <>
+                  <span className={classes.menuIcon}>
+                    <IcoCrew onGreen={selectColor(isActive)} />
+                  </span>
+                  <span
+                    className={classes.menuName}
+                    style={{
+                      color: selectColor(isActive),
+                    }}
+                  >
+                    크루
+                  </span>
+                </>
+              )}
+            </NavLink>
           </li>
           <li className={classes.listMenu}>
-            <Link to="/record">
-              <span className={classes.menuIcon}>
-                <img src={icoRecord} />
-              </span>
-              <span
-                className={classes.menuName}
-                style={{
-                  color: selectColor("/record"),
-                }}
-              >
-                기록
-              </span>
-            </Link>
+            <NavLink to="/record">
+              {({ isActive }) => (
+                <>
+                  <span className={classes.menuIcon}>
+                    <img src={icoRecord} />
+                  </span>
+                  <span
+                    className={classes.menuName}
+                    style={{
+                      color: selectColor(isActive),
+                    }}
+                  >
+                    기록
+                  </span>
+                </>
+              )}
+            </NavLink>
           </li>
           <li className={classes.listMenu}>
             <Link to="/">
@@ -63,34 +69,42 @@ function Bottom() {
             </Link>
           </li>
           <li className={classes.listMenu}>
-            <Link to="/challenge">
-              <span className={classes.menuIcon}>
-                <IcoChallenge onGreen={selectColor("/challenge")} />
-              </span>
-              <span
-                className={classes.menuName}
-                style={{
-                  color: selectColor("/challenge"),
-                }}
-              >
-                챌린지
-              </span>
-            </Link>
+            <NavLink to="/challenge">
+              {({ isActive }) => (
+                <>
+                  <span className={classes.menuIcon}>
+                    <IcoChallenge onGreen={selectColor(isActive)} />
+                  </span>
+                  <span
+                    className={classes.menuName}
+                    style={{
+                      color: selectColor(isActive),
+                    }}
+                  >
+                    챌린지
+                  </span>
+                </>
+              )}
+            </NavLink>
           </li>
           <li className={classes.listMenu}>
-            <Link to="mypage">
-              <span className={classes.menuIcon}>
-                <IcoPerson onGreen={selectColor("/mypage")} />
-              </span>
-              <span
-                className={classes.menuName}
-                style={{
-                  color: selectColor("/mypage"),
-                }}
-              >
-                마이페이지
-              </span>
-            </Link>
+            <NavLink to="/mypage">
+              {({ isActive }) => (
+                <>
+                  <span className={classes.menuIcon}>
+                    <IcoPerson onGreen={selectColor(isActive)} />
+                  </span>
+                  <span
+                    className={classes.menuName}
+                    style={{
+                      color: selectColor(isActive),
+                    }}
+                  >
+                    마이페이지
+                  </span>
+                </>
+              )}
+            </NavLink>
           </li>
         </ul>
       </div>
