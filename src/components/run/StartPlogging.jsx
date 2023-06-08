46b11ffaff56d231d1864bab2559219459f75ea5
jeha-dev <leejeha.dev@gmail.com>
2023-06-08 13:47:49 +0900

    플로깅 시작버튼 관련 수정

--- src/components/run/StartPlogging.jsx
+++ src/components/run/StartPlogging.jsx
@@ -79,9 +79,9 @@ function StartPlogging() {
               </div>
             </div>
             <div className={`dsFlex btnBox`}>
+              {/* 0608 시작버튼으로 수정 */}
               <button type="button" className={`btnCircle`}>
-                <span className="hide">일시정지</span>
-                <IcoPause />
+                시작
               </button>
             </div>
           </div>
