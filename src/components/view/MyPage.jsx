20cd6a4c0ae027fafcb32f484f4d9c00b10e5c0f
LeeJams <woals214@gmail.com>
2023-06-08 17:32:27 +0900

    swiper

--- src/components/view/MyPage.jsx
+++ src/components/view/MyPage.jsx
@@ -1,22 +1,34 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper";
+import { Autoplay, Pagination } from "swiper";
 import "swiper/css";
+import "swiper/css/pagination";
+import "./MyPage.css";
 
 export default function MyPage() {
   return (
-    <div>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      {/* https://swiperjs.com/demos 참고.... */}
       <Swiper
-        modules={[Autoplay]}
-        spaceBetween={50}
-        slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-        autoplay={{ delay: 3000 }}
+        pagination={true}
+        modules={[Autoplay, Pagination]}
+        autoplay={{ delay: 2000 }}
+        className="mySwiper"
+        loop={true}
       >
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </div>
   );
