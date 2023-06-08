99211ce72c44a2ac9844e11d0735cbf5e1255c66
jeha-dev <leejeha.dev@gmail.com>
2023-06-08 15:14:17 +0900

    크루 랜딩 페이지 업로드

---
+++ src/components/ui/BtnAdd.jsx
@@ -0,0 +1,39 @@
+function BtnAdd() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24">
+      <g transform="translate(-51 -319)">
+        <circle
+          cx="12"
+          cy="12"
+          r="12"
+          transform="translate(51 319)"
+          fill="#1ec41e"
+        />
+        <g transform="translate(0 -13.75)">
+          <g
+            transform="translate(62.25 338.75)"
+            fill="#fff"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-width="1"
+          >
+            <rect width="1.5" height="12" stroke="none" />
+            <rect x="0.5" y="0.5" width="0.5" height="11" fill="none" />
+          </g>
+          <g
+            transform="translate(69 344) rotate(90)"
+            fill="#fff"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-width="1"
+          >
+            <rect width="1.5" height="12" stroke="none" />
+            <rect x="0.5" y="0.5" width="0.5" height="11" fill="none" />
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default BtnAdd;
