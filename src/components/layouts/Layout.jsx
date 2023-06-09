58e711bb3e96c07dddbe30c65e2fd43b3209f00f
jeha-dev <leejeha.dev@gmail.com>
2023-06-09 11:10:47 +0900

    브랜치 생성

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 import Bottom from "./Bottom";
 // import Running from "../run/StartPlogging";
 // import PopScan from "../run/PopScan";
-import Crew from "../view/Crew";
+// import Crew from "../view/Crew";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
