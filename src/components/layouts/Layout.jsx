f9e02411f5e5ff47fda2f509860d1ae2cb0c60cc
LeeJams <woals214@gmail.com>
2023-06-09 23:48:00 +0900

    to Top

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,8 +1,15 @@
 import Header from "./Header";
 import Bottom from "./Bottom";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <div id="layoutWrap">
       <Header />
