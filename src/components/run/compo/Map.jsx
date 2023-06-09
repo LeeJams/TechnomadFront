0bb16b38278b959a7799fffe2c268acbc8af36a1
LeeJams <woals214@gmail.com>
2023-06-10 00:27:43 +0900

    temp

--- src/components/run/compo/Map.jsx
+++ src/components/run/compo/Map.jsx
@@ -21,7 +21,8 @@ export default function Map() {
         navigate(-1);
       }
     };
-    getCurLocation();
+    // getCurLocation();
+    temp();
   }, [navigate]);
 
   const success = (position) => {
@@ -36,6 +37,16 @@ export default function Map() {
 
     let kakaoMap = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
     setMap(kakaoMap);
+  };
+
+  const temp = (position) => {
+    let mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
+
+    let kakaoMap = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
   };
 
   useEffect(() => {
